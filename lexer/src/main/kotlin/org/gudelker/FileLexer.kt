0400485e0f8649e1d607254eeb542617fd8cfbf1
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-08-17 19:40:52 -0300

    Started Parser

--- lexer/src/main/kotlin/org/gudelker/FileLexer.kt
+++ lexer/src/main/kotlin/org/gudelker/FileLexer.kt
@@ -5,47 +5,44 @@ import org.gudelker.result.Result
 import org.gudelker.result.SyntaxError
 import org.gudelker.result.Valid
 
-class FileLexer(val rules : List<RuleTokenizer>) : Lexer {
+class FileLexer(val rules: List<RuleTokenizer>) : Lexer {
 
     override fun lex(fileName: String): Result {
         val reader = Reader(fileName)
-        var actualWord = ""
-        var tokensList = listOf<Token>()
-        var startPos = Position()
 
-        while (!reader.isEOF()) {
-            val newChar = reader.next().toString()
-            actualWord += newChar
-            val nextChar = reader.peek()
-            if (nextChar == '\n' || nextChar == '\r') {
-                advancePosition(startPos, nextChar)
+        fun lexRec(
+            actualWord: String,
+            tokensList: List<Token>,
+            startPos: Position
+        ): Result {
+            if (reader.isEOF()) {
+                val finalTokens = tokensList + Token(TokenType.EOF, "EOF", startPos)
+                return Valid(finalTokens)
             }
+            val newChar = reader.next().toString()
+            val updatedWord = actualWord + newChar
+            val nextChar = reader.peek()
+            val updatedPos = if (nextChar == '\n' || nextChar == '\r') {
+                advancePosition(startPos, nextChar)
+            } else startPos
 
-            for (rule in rules) {
-                if (rule.matches(actualWord, nextChar)) {
-                    val posWithNewOffset = changingOffSet(startPos, actualWord)
-                    val pos = posWithNewOffset.copy()
-                    tokensList = rule.generateToken(tokensList, actualWord, pos)
-                    val lastToken = tokensList.last()
-                    val lastTokenIsUnknown = lastToken.getType() == TokenType.UNKNOWN
-                    if (lastTokenIsUnknown){
-                        return SyntaxError("Syntax error in line: " +
-                                lastToken.getPosition().startLine)
-                    }
-                    startPos = advancePosition(posWithNewOffset, nextChar)
-                    actualWord = resetWordToEmpty()
-                    break
+            val matchingRule = rules.firstOrNull { it.matches(updatedWord, nextChar) }
+            return if (matchingRule != null) {
+                val posWithNewOffset = changingOffSet(updatedPos, updatedWord)
+                val pos = posWithNewOffset.copy()
+                val newTokens = matchingRule.generateToken(tokensList, updatedWord, pos)
+                val lastToken = newTokens.last()
+                if (lastToken.getType() == TokenType.UNKNOWN) {
+                    SyntaxError("Syntax error in line: ${lastToken.getPosition().startLine}")
+                } else {
+                    lexRec("", newTokens, advancePosition(posWithNewOffset, nextChar))
                 }
+            } else {
+                lexRec(updatedWord, tokensList, updatedPos)
             }
         }
-        var finalTokenList = tokensList.toMutableList()
-        finalTokenList.add(Token(TokenType.EOF,"EOF", startPos))
-        val resultValue = finalTokenList
-        return Valid(resultValue)
-    }
 
-    private fun resetWordToEmpty(): String {
-        return ""
+        return lexRec("", listOf(), Position())
     }
 
     private fun advancePosition(
@@ -53,27 +50,18 @@ class FileLexer(val rules : List<RuleTokenizer>) : Lexer {
         char: Char?
     ): Position {
         return when (char) {
-            '\n' -> position.copy(
+            '\n', '\r' -> position.copy(
                 endOffset = -1,
                 startLine = position.endLine + 1,
                 startColumn = 0,
                 endColumn = 0,
                 endLine = position.endLine + 1
             )
-            '\r' -> position.copy(
-                endOffset = -1,
-                startLine = position.endLine + 1,
-                startColumn = 0,
-                endColumn = 0,
-                endLine = position.endLine + 1
-            )
-
             else -> position.copy(
                 startOffset = position.endOffset + 1,
                 endOffset = position.endOffset + 1,
                 endColumn = position.endColumn + 1,
                 startColumn = position.endColumn + 1
-
             )
         }
     }
--- parser/src/main/kotlin/org/gudelker/DefaultParser.kt
+++ parser/src/main/kotlin/org/gudelker/DefaultParser.kt
@@ -4,25 +4,25 @@ import org.example.Statement
 import org.gudelker.components.org.gudelker.TokenType
 
 class DefaultParser(val list: List<Token>, val root: List<Statement>) {
-    fun parse(): DefaultParser {
-        fun parseRec(index: Int, acc: List<Statement>): DefaultParser {
-            if (index >= list.size || list[index].getType() == TokenType.EOF) {
-                return DefaultParser(list.subList(index, list.size), acc)
-            }
-            val token = list[index]
-            // Replace with your actual statement parsing logic:
-            if (token.getType() == TokenType.SEMICOLON) {
-                return parseRec(index + 1, acc)
-            }
-            val statement = parseStatement(token) // Implement this function
-            return parseRec(index + 1, acc + statement)
-        }
+    fun parse(): List<Statement> {
         return parseRec(0, root)
     }
 
     private fun parseStatement(token: Token): Statement {
         // TODO: Implement actual statement parsing logic
         throw NotImplementedError("parseStatement not implemented")
+    }
+    private fun parseRec(index: Int, ast: List<Statement>): List<Statement> {
+        if (index >= list.size || list[index].getType() == TokenType.EOF) {
+            return ast
+        }
+        val token = list[index]
+        if (token.getType() == TokenType.SEMICOLON) {
+            return parseRec(index + 1, ast)
+        }
+        val statement = parseStatement(token)
+        val newRoot = ast + statement
+        return parseRec(index + 1, newRoot)
     }
 
     fun getRoot(): List<Statement> = root
