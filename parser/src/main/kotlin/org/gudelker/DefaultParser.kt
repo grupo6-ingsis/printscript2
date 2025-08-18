15525e4936734034a134c5478c28943bddcb4464
Agustín Guglielmelli <agusguglielmelli@gmail.com>
2025-08-18 14:30:59 -0300

    .

--- parser/src/main/kotlin/org/gudelker/DefaultParser.kt
+++ parser/src/main/kotlin/org/gudelker/DefaultParser.kt
@@ -5,24 +5,24 @@ import org.gudelker.components.org.gudelker.TokenType
 
 class DefaultParser(val list: List<Token>, val root: List<Statement>) {
     fun parse(): List<Statement> {
-        return parseRec(0, root)
+        return parseRecursive(0, root)
     }
 
     private fun parseStatement(token: Token): Statement {
         // TODO: Implement actual statement parsing logic
         throw NotImplementedError("parseStatement not implemented")
     }
-    private fun parseRec(index: Int, ast: List<Statement>): List<Statement> {
+    private fun parseRecursive(index: Int, ast: List<Statement>): List<Statement> {
         if (index >= list.size || list[index].getType() == TokenType.EOF) {
             return ast
         }
         val token = list[index]
         if (token.getType() == TokenType.SEMICOLON) {
-            return parseRec(index + 1, ast)
+            return parseRecursive(index + 1, ast)
         }
         val statement = parseStatement(token)
         val newRoot = ast + statement
-        return parseRec(index + 1, newRoot)
+        return parseRecursive(index + 1, newRoot)
     }
 
     fun getRoot(): List<Statement> = root
