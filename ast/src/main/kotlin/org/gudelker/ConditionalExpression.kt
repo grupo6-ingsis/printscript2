e9936daa24d15628e251b394223bfb2348ef2819
MarcoSchenker <mschenker@mail.austral.edu.ar>
2025-09-04 02:50:26 -0300

    added ifexpression and its rule

--- ast/src/main/kotlin/org/gudelker/ConditionalExpression.kt
+++ ast/src/main/kotlin/org/gudelker/ConditionalExpression.kt
@@ -1,3 +1,10 @@
 package org.gudelker
 
-class ConditionalExpression
+import org.gudelker.stmtposition.ComboValuePosition
+
+data class ConditionalExpression(
+    val ifKeyword: ComboValuePosition<String>,
+    val condition: BooleanExpressionStatement,
+    val ifBody: List<Statement>,
+    val elseBody: List<Statement>? = null,
+) : ConditionalStatement
