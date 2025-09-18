4d88a62ed35db87180930739b4c098bee4d95c29
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-09-18 00:33:23 -0300

    deleted read provider not using it

--- interpreter/src/main/kotlin/org/gudelker/inputprovider/ReadProvider.kt
+++
@@ -1,8 +0,0 @@
-package org.gudelker.inputprovider
-
-class ReadProvider(private val value: String) : InputProvider {
-    override fun nextInput(prompt: String): String {
-        print(prompt)
-        return value
-    }
-}
