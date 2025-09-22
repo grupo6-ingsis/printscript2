52a0d6ad05f415ec8d5edeb72e1e960adf682fa4
pedrodelaguila <pdelaguila@mail.austral.edu.ar>
2025-09-22 15:45:45 -0300

    Last version

--- analyzer/src/test/kotlin/org/gudelker/YamlLinterConfigLoaderTest.kt
+++ analyzer/src/test/kotlin/org/gudelker/YamlLinterConfigLoaderTest.kt
@@ -7,7 +7,7 @@ class YamlLinterConfigLoaderTest {
     fun `loads config from existing linterconfig yaml`() {
         val loader =
             YamlLinterConfigLoaderToMap(
-                "/Users/pedrodelaguila/faculty/ingsis/printscript2/analyzer/src/main/kotlin/org/gudelker/linterconfig.yaml",
+                "src/main/kotlin/org/gudelker/linterconfig.yaml",
             )
         val configMap = loader.loadConfig()
         assertEquals("camelCase", configMap["identifierFormat"]?.identifierFormat)
